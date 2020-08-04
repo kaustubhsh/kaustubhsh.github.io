f67f6ab7ec245382dfec19bac7e4f6488f5bd6f9
Kaustubh Sharma <kaustubhsharma97@gmail.com>
2020-08-05 01:42:33 +0530

    Updates

---
+++ precache-manifest.fbff88be2f83af847acfbdc6e5a4f0f8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cef74bd87e997a5163dbe761dd20649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16fad49c52bdce0b9efa",
+    "url": "/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "a4f1a0778030805c0d40",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "d1710c6df0fbfc7ff31f",
+    "url": "/static/css/main.b6cfba51.chunk.css"
+  },
+  {
+    "revision": "7c067e5ed27e5c076139",
+    "url": "/static/js/2.e1c671a8.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.e1c671a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16fad49c52bdce0b9efa",
+    "url": "/static/js/3.fb79f0a4.chunk.js"
+  },
+  {
+    "revision": "a4f1a0778030805c0d40",
+    "url": "/static/js/4.9415793d.chunk.js"
+  },
+  {
+    "revision": "d1710c6df0fbfc7ff31f",
+    "url": "/static/js/main.e744bba7.chunk.js"
+  },
+  {
+    "revision": "ff16e802a42b0d9c49a8",
+    "url": "/static/js/runtime-main.ad3ed0aa.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "df7962b99b19f6a26c6f6ee433f26c4f",
+    "url": "/static/media/adhocLogo.df7962b9.jpeg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/static/media/developer.a73310ea.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e6e3d93b2c82fbc33230a0132b2c3d03",
+    "url": "/static/media/gurugramLogo.e6e3d93b.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
